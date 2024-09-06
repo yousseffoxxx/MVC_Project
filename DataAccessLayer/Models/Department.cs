639cb91a38456bd62dc8becf1aa19a168050ce8a
Youssef Ahmed <youssef5fox5@gmail.com>
2024-09-06 14:50:17 +0300

    Part 07 PL [Department Index View ]

--- DataAccessLayer/Models/Department.cs
+++ DataAccessLayer/Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace DataAccessLayer.Models
         public int Id { get; set; }
         public int Code { get; set; }
         public string Name { get; set; }
+        [Display(Name= "Created At")]
         public DateTime CDate { get; set; }
     }
 }
