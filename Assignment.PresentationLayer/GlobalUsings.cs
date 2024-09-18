c35a855a3f7219d902be936f355b5331533e5af7
Youssef Ahmed <youssef5fox5@gmail.com>
2024-09-18 16:19:27 +0300

    Editing Conflicted Data For Videos From 1 to 5

--- Assignment.PresentationLayer/GlobalUsings.cs
+++ Assignment.PresentationLayer/GlobalUsings.cs
@@ -4,7 +4,6 @@ global using DataAccessLayer.Data;
 global using DataAccessLayer.Models;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
 global using Microsoft.AspNetCore.Mvc.Rendering;
 global using Assignment.PresentationLayer.ViewModels;
 global using System.Diagnostics;
@@ -12,7 +11,4 @@ global using System.ComponentModel.DataAnnotations;
 global using PresentationLayer.ViewModels;
 global using AutoMapper;
 global using PresentationLayer.Utilities;
-=======
-global using System.ComponentModel.DataAnnotations;
 global using PresentationLayer.Models;
->>>>>>> a3a3455 (Part 04 Account Controller Register)
